104d78592ab482553af8cb18fcbdd83ed0bac801
Abdelhaq <tigha66@gmail.com>
2026-02-18 07:34:58 +0000

    Add HealthHub PWA with real features

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'healthhub-v1';
+const urlsToCache = [
+  '/healthhub-pwa.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) return caches.delete(cache);
+        })
+      );
+    })
+  );
+});
